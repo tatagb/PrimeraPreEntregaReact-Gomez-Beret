a5fc2da0bf9278563b42e39b506278fabe84248f
Federico Gomez Beret <federicogomezberet@gmail.com>
2023-03-23 22:19:49 -0300

    Cambios

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfzTrJW0Piett4iDWsoMjryVsbRpRs-Dw",
+  authDomain: "federicogbcommerce.firebaseapp.com",
+  projectId: "federicogbcommerce",
+  storageBucket: "federicogbcommerce.appspot.com",
+  messagingSenderId: "310334070001",
+  appId: "1:310334070001:web:e89c36c728ed0831a19d50"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
